f073305d9aff9fbe455d85e12524ed0f82c3bffb
Maycon Douglas <crossfireelite601@gmail.com>
2023-01-21 21:15:51 -0300

    first commit

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const Api = axios.create({
-  baseURL: 'https://chatappbackend-production-45fa.up.railway.app/',
+  baseURL: 'https://chatappbackend-production-45fa.up.railway.app',
 })
 
 export default Api
